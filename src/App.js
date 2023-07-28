727bd95757acca0ba2f6b43d9b4172dd28d318cb
GHERICK LADIGNON <gherick.ladignon@tfisolutions.com>
2023-07-28 18:30:09 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-red-700">
     Hello world!
   </h1>
   );
